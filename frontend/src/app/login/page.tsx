9c82f6244907cd44e77adad8b8a16759ca98853c
LaiAnTan <tanlaian2004@gmail.com>
2025-01-01 02:30:33 +0800

    feat: design for login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,43 +1,53 @@
 "use client"
 
 import { redirect } from "next/navigation"
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
 
-function DiscordOAuth() {
+function OAuthButton({ provider, icon_src }: { provider: string, icon_src: string }) {
 
-  const login = () => {
-    redirect('http://localhost:8000/oauth/discord')
-  }
-
-  return (<div className="cursor-pointer select-none" onClick={login}>Login with Discord</div>)
+  return (
+    <button
+      className="flex flex-row bg-white rounded-lg h-8 px-4 space-x-3 items-center hover:scale-105 hover:drop-shadow-sm transition-all"
+      onClick={() => {redirect(`http://localhost:8000/oauth/${provider}`)}}>
+      <Image className="w-5 h-auto" src={icon_src} alt={provider} width={20} height={64}/>
+      <div className="text-black text-xs">Continue with {provider.charAt(0).toUpperCase() + provider.slice(1)}</div>
+    </button>
+  )
 }
 
-function Intra42OAuth() {
-
-  const login = () => {
-    redirect('http://localhost:8000/oauth/42')
-  }
-
-  return (<div className="cursor-pointer select-none" onClick={login}>Login with 42 Intra</div>)
+function SignInCard() {
+  
+  return (
+    <div className="flex flex-col w-3/4 lg:w-1/3 h-auto bg-gradient-to-br from-purple-200 to-[#9EFCFF] rounded-lg drop-shadow-lg px-12 lg:px-20 pb-12 pt-8 space-y-4">
+      <div className="text-black text-2xl font-semibold">Sign In</div>
+      <input className="h-8 bg-white rounded-lg p-2 text-black text-xs" placeholder="Email"/>
+      <input className="h-8 bg-white rounded-lg p-2 text-black text-xs" placeholder="Password"/>
+      <button className="h-8 bg-purple-400 rounded-lg p-1 font-bold font-white text-sm hover:scale-105 hover:drop-shadow-sm transition-all">Sign In</button>
+      <div className="flex flex-row space-x-2 justify-center items-center">
+         <hr className="w-1/2 border-black"/>
+         <div className="text-black text-sm font-semibold">or</div>
+         <hr className="w-1/2 border-black"/>
+      </div>
+      <OAuthButton provider="42" icon_src="/42.svg" />
+      <OAuthButton provider="discord" icon_src="/discord.svg" />
+      <OAuthButton provider="github" icon_src="/github.svg" />
+      <div className="flex flex-row text-black text-[0.6rem] space-x-2 items-center justify-center">
+        <Link href="/login" className="text-blue-500 underline cursor-pointer">Register</Link>
+        <div>or</div>
+        <Link href="/login" className="text-blue-500 underline cursor-pointer">Reset Password</Link>
+      </div>
+    </div>
+  )
 }
-
-function GitHubOAuth() {
-
-  const login = () => {
-    redirect('http://localhost:8000/oauth/github')
-  }
-
-  return (<div className="cursor-pointer select-none" onClick={login}>Login with GitHub</div>)
-}
-
 
 export default function Login() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-white text-black">
-      <div className="font-bold">This is the login page</div>
-      <DiscordOAuth />
-      <Intra42OAuth />
-      <GitHubOAuth />
+    <div className="flex flex-col h-screen w-screen bg-white items-center justify-center space-y-4">
+      <div className="font-bold text-black">hypertube</div>
+      <SignInCard />
     </div>
   )
 }
