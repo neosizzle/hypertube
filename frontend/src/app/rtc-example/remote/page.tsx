ddc0789e2ea9a01d490d825dc0a58e8a5ce9e239
jng <junhanngd1115@gmail.com>
2025-01-20 12:53:01 +0800

    feat: requirements.txt

---
+++ frontend/src/app/rtc-example/remote/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function RtcExampleRemote() {
+
+  return (
+    <div className="flex flex-col h-screen w-screen bg-white items-center justify-center space-y-4">
+      <div className="font-bold text-black text-base lg:text-2xl">remote RTC</div>
+      
+    </div>
+  )
+}
